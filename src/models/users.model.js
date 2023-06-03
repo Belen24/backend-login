bd65d11936b251f7e9720661d12efc6bdfc3d896
Belen <bgutierrez.mil@gmail.com>
2023-06-03 00:40:36 -0400

    desafio.login

---
+++ src/models/users.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const usersCollection = "users";
+
+const usersSchema = new mongoose.Schema({
+    first_name:{type:String,required:true},
+    last_name:{type:String,required:true},
+    age:{type:Number,required:true},
+    email:{type:String,required:true, unique:true},
+    password:{type:String, required:true},
+    role: {type: String, default: "usuario"}
+});
+
+export const userModel = mongoose.model(usersCollection,usersSchema);
---
+++ src/routes/views.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { productsModel } from "../models/products.model.js";
+
+const router = Router();
+
+
+//rutas de las vistas
+router.get("/", (req,res)=>{
+    res.render("home");
+});
+
+router.get("/login", (req,res)=>{
+    res.render("login");
+});
+
+router.get("/signup", (req,res)=>{
+    res.render("registro");
+});
+
+router.get("/profile", (req,res)=>{
+    console.log(req.session.user)
+    res.render("perfil");
+    //res.render ("perfil");
+});
+
+router.get("/products", async (req, res) => {
+    try {
+      console.log(req.session.user);
+      const products = await productsModel.find().lean();
+      res.render("products", { email: req.session.user.email, products: products });
+    } catch (error) {
+      console.log(error);
+      res.send("Hubo un error al cargar los productos");
+    }
+  });
+
+
+export { router as viewsRouter};
